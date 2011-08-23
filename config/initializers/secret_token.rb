bfb3e64c756d5102078733b82d96dd1923c5a58b
mskrishna <sivakrishna.motamarri@gmail.com>
2011-08-23 16:05:34 +0530

    just app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MultipleConnectors::Application.config.secret_token = '385dc157e63fff9757b04c15150701aabda71984ebc1a642dc8df2c8e1e93ec8250cfaf378e227319cf277d9af29ef3a6625bd10874cac17ff32de36257cc0c5'
